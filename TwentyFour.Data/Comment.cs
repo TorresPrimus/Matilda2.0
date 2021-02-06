be303c38074df6781d58df976066840c84c70251
Richard Torres <torresrichard252@gmail.com>
2021-02-06 10:43:28 -0500

    Add project files.

---
+++ TwentyFour.Data/Comment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyFour.Data
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [ForeignKey(nameof(Author))]
+        public Guid Author { get; set; }
+        public virtual User AuthorId { get; set; }
+
+        [ForeignKey(nameof(CommentPost))]
+        public string CommentPostId { get; set; }
+        public virtual Post CommentPost { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
---
+++ TwentyFour.Models/CommentListItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyFour.Models
+{
+    public class CommentListItem
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public Guid Author { get; set; }
+
+        [Display(Name="Replies")]
+        public string Reply { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
