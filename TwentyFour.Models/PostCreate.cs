cc21f36d4059931f521e7f59f6e3cec9c12c51e1
Richard Torres <torresrichard252@gmail.com>
2021-02-06 16:18:42 -0500

    FINALx8 FORM Migrations Issues

--- TwentyFour.Models/PostCreate.cs
+++ TwentyFour.Models/PostCreate.cs
@@ -17,8 +17,8 @@ namespace TwentyFour.Models
         [Required]
         [MaxLength(8000)]
         public string Text { get; set; }
-        [Required]
-        public Guid AuthorID { get; set; }
+        //[Required]
+        //public Guid AuthorID { get; set; }
 
     }
 }
